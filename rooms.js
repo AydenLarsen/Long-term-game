4d464ce627aefdbbb2ed1038662cb9bbe395dc83
AydenLarsen <124610038+AydenLarsen@users.noreply.github.com>
2023-06-09 09:54:34 -0600

    rooms.js

--- rooms.js
+++ rooms.js
@@ -7,7 +7,42 @@ function start() {
   ctx.fillText("CLICK TO START", 350, 285);
 }
 
-function room() {
+function house() {
+  mainComponents();
+  housecollisions();
+
+  if (player.x + player.w > 799) {
+    player.x = 5;
+    player.y = 275;
+    townEl.x = 0;
+    state = "town";
+  }
+}
+
+function town() {
+  mainComponents();
+  towncollisions();
+
+  if (
+    player.x < 1 &&
+    player.y + townEl.y > 269 &&
+    player.y + player.h + townEl.y < 331
+  ) {
+    state = "house";
+    player.x = 745;
+    player.y = 275;
+  } else if (
+    player.x + player.w > 799 &&
+    player.y + townEl.y > 269 &&
+    player.y + player.h < 331
+  ) {
+    state = "enemyroom";
+    player.x = 5;
+    player.y = 275;
+  }
+}
+
+function enemyroom() {
   mainComponents();
   collisions();
 
@@ -21,10 +56,14 @@ function room() {
     player.y < 350
   ) {
     state = "enemy";
+  } else if (player.x < 1) {
+    state = "town";
+    player.x = 745;
+    townEl.x = -800;
   }
 }
 
-function room2() {
+function enemyroom2() {
   mainComponents();
   collisions();
 
@@ -59,7 +98,37 @@ function win() {
 }
 
 function mainComponents() {
-  if (state === "room") {
+  if (state === "house") {
+    ctx.fillStyle = "burlywood";
+    ctx.fillRect(0, 0, cnv.width, cnv.height);
+
+    ctx.fillStyle = "gray";
+    ctx.fillRect(50, 50, cnv.width - 100, cnv.height - 100);
+    ctx.fillRect(750, 250, 50, 100);
+
+    ctx.drawImage(characterImg, player.x, player.y, player.h, player.h);
+  } else if (state === "town") {
+    ctx.fillStyle = "darkgray";
+    ctx.fillRect(0, 0, 1600, 1200);
+
+    ctx.fillStyle = "burlywood";
+    ctx.fillRect(townEl.x + 0, townEl.y + 200, 50, 200);
+
+    ctx.fillStyle = "brown";
+    ctx.fillRect(townEl.x + 0, townEl.y + 270, 50, 60);
+
+    ctx.fillStyle = "green";
+    ctx.fillRect(townEl.x + 1550, townEl.y + 200, 50, 200);
+
+    ctx.fillStyle = "sandybrown";
+    ctx.fillRect(townEl.x + 1550, townEl.y + 270, 50, 60);
+
+    ctx.font = "20px Consolas";
+    ctx.fillStyle = "black";
+    ctx.fillText("Home", townEl.x + 60, townEl.y + 305);
+
+    ctx.drawImage(characterImg, player.x, player.y, player.h, player.h);
+  } else if (state === "enemyroom") {
     ctx.fillStyle = "green";
     ctx.fillRect(0, 0, cnv.width, cnv.height);
 
@@ -71,7 +140,7 @@ function mainComponents() {
 
     ctx.fillStyle = "red";
     ctx.fillRect(700, 250, 100, 100);
-  } else if (state === "room2") {
+  } else if (state === "enemyroom2") {
     ctx.fillStyle = "darkgray";
     ctx.fillRect(0, 0, cnv.width, cnv.height);
 
