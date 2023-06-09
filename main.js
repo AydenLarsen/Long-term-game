2c68788737162448324fe675b0e8849fd6c7d8d0
AydenLarsen <124610038+AydenLarsen@users.noreply.github.com>
2023-06-09 09:54:53 -0600

    main.js

--- main.js
+++ main.js
@@ -15,13 +15,15 @@ let dmgRoll = 0;
 let enemyDmgRoll = 0;
 let dmgDealt = 0;
 let enemyDmgDealt = 0;
+let dmgMult = 1;
+let enemyDmgMult = 1;
 let enemyattacktext;
 let slap = document.createElement("audio");
 slap.src = "audio/slap.mp3";
 let characterImg = document.createElement("img");
 characterImg.src = "img/character.jpg";
 let player = {
-  x: 0,
+  x: 50,
   y: 275,
   w: 50,
   h: 50,
@@ -33,6 +35,10 @@ let player = {
 };
 let entity = {
   hp: 100,
+};
+let townEl = {
+  x: 0,
+  y: 0,
 };
 let gameoverimg = document.createElement("img");
 gameoverimg.src = "img/funny.jpg";
